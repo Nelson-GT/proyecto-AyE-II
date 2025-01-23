788395f0fcc62f881f64815c98702def8b756281
Nelson <nelsonjesusGT@gmail.com>
2025-01-22 20:31:26 -0400

    Cambio en la funcion crearHash menor

--- commit.py
+++ commit.py
@@ -8,7 +8,8 @@ class Commit:
         self.anterior = anterior
         self.hash = self.crear_Hash()
     
-    def crear_Hash():                           #función para crear el hash aleatorio del commit
+    
+    def crear_Hash(self):                           #función para crear el hash aleatorio del commit
         letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         hash = ""
         for k in range(12):             #añade a hash un elemento aleatorio de la lista para crear un hash de 12 caracteres
